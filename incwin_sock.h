8eef5028ded3cbca5d1916b81e3cea61fe3105fd
Kamran <121111247+dev-kamran2001@users.noreply.github.com>
2022-12-20 18:42:59 +0330

    Add files via upload

---
+++ incwin_sock.h
@@ -0,0 +1,13 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <Windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
